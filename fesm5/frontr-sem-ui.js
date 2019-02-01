f0cd60d27d2de91b8b75479ce25cb893a1764136
Front End <chris@frontr.co.uk>
2019-02-01 08:37:51 +0000

    Introducing demo-app scss

--- fesm5/frontr-sem-ui.js
+++ fesm5/frontr-sem-ui.js
@@ -1,4 +1,4 @@
-import { Injectable, ChangeDetectionStrategy, Component, HostBinding, Input, ViewEncapsulation, HostListener, NgModule, ContentChild, TemplateRef, Output, EventEmitter, ElementRef, Renderer2, ChangeDetectorRef, ViewChild, Directive, ContentChildren, defineInjectable } from '@angular/core';
+import { Injectable, ChangeDetectionStrategy, Component, HostBinding, Input, ViewEncapsulation, HostListener, ContentChild, TemplateRef, NgModule, Output, EventEmitter, ElementRef, Renderer2, ChangeDetectorRef, ViewChild, Directive, ContentChildren, defineInjectable } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Overlay, OverlayModule, ConnectionPositionPair } from '@angular/cdk/overlay';
 import { Subject, fromEvent } from 'rxjs';
